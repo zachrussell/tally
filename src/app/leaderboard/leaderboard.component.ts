b0d1193921a0e8f7ebd63e0ed25d93508c8ba00a
Zach Russell <zachary.russell@accenture.com>
2017-08-18 10:24:52 -0600

    added gradients

--- src/app/leaderboard/leaderboard.component.ts
+++ src/app/leaderboard/leaderboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges} from '@angular/core';
 import { ScoreService } from '../score-service.service';
 
 @Component({
@@ -10,11 +10,55 @@ export class LeaderboardComponent implements OnInit {
 
   @Input() title = 'Leader Board';
   @Input() players = [];
-
+  public gradients = null;
   constructor(private _scoreService: ScoreService) { }
 
 
   ngOnInit() {
+     this.setGradients();
+  }
+
+  ngOnChanges(changes: any) {
+    this.setGradients();
+  }
+
+  setGradients() {
+    this.gradients = this.gradient('#fa6855', '#c24448', this.players.length);
+  }
+
+  gradient(startColor, endColor, steps) {
+      const start = {
+              'Hex'   : startColor,
+              'R'     : parseInt(startColor.slice(1, 3), 16),
+              'G'     : parseInt(startColor.slice(3, 5), 16),
+              'B'     : parseInt(startColor.slice(5, 7), 16)
+      };
+
+      const end = {
+              'Hex'   : endColor,
+              'R'     : parseInt(endColor.slice(1, 3), 16),
+              'G'     : parseInt(endColor.slice(3, 5), 16),
+              'B'     : parseInt(endColor.slice(5, 7), 16)
+      };
+
+      const diffR = end['R'] - start['R'];
+      const diffG = end['G'] - start['G'];
+      const diffB = end['B'] - start['B'];
+
+      const stepsHex  = new Array();
+      const stepsR    = new Array();
+      const stepsG    = new Array();
+      const stepsB    = new Array();
+
+      for (let i = 0; i <= steps; i++) {
+              stepsR[i] = start['R'] + ((diffR / steps) * i);
+              stepsG[i] = start['G'] + ((diffG / steps) * i);
+              stepsB[i] = start['B'] + ((diffB / steps) * i);
+              stepsHex[i] = '#' + Math.round(stepsR[i]).toString(16) + '' +
+                                  Math.round(stepsG[i]).toString(16) + '' +
+                                  Math.round(stepsB[i]).toString(16);
+      }
+      return stepsHex;
 
   }
 
